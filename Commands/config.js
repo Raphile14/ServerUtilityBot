fca3a833987d7a6c3ab04c325e000426f14f52e9
Raphael Dalangin <60991234+Raphile14@users.noreply.github.com>
2020-09-02 12:47:16 +0800

    Delete config.js

--- Commands/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'config',
-    description: 'Server Configurations',
-    bot_name: 'Raphael\'s Server Utility Bot',
-    prefix: '!rsub',
-    password: 'NzUwNDk2ODUwNzMzMTcwODA5.X07YqA.P8vXMr1RYRw4lA0zMdSfG4pFmYA'
-}
