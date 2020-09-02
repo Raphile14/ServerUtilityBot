10cf4ba8130034eeb23c5d12c2f912dda257c7b4
Raphile14 <rllddalangin@gmail.com>
2020-09-02 12:44:34 +0800

    indev 0.0.1 (September 2, 2020)

---
+++ Commands/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'config',
+    description: 'Server Configurations',
+    bot_name: 'Raphael\'s Server Utility Bot',
+    prefix: '!rsub',
+    password: 'NzUwNDk2ODUwNzMzMTcwODA5.X07YqA.P8vXMr1RYRw4lA0zMdSfG4pFmYA'
+}
